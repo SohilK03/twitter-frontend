834e364826f6741a2da2c7cd6db2090aa0978b62
Sohil Khattar <sohilkhattar123@gmail.com>
2022-08-19 10:14:33 +0530

    twitter home completed

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,8 +1,21 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 import { connect } from "react-redux";
+import Sidebar from "../Components/Sidebar/Sidebar";
+import Feed from "../Components/Feed/Feed";
+import Widgets from "../Components/Widgets/Widgets";
+import "./HomePage.css";
 function HomePage(props) {
-  return props.user.isLoggedIn ? <div>Hello, Hello</div> : <Navigate to="/" />;
+  return props.user.isLoggedIn ? (
+    <div className="home">
+      {" "}
+      <Sidebar />
+      <Feed />
+      <Widgets />
+    </div>
+  ) : (
+    <Navigate to="/" />
+  );
 }
 
 const mapStateToProps = (state) => ({ user: state.user });
